9d87e57cbed97ef1164d4b93ff5ec7aa44c6ddbe
Pavel Martynov <martynovdev@gmail.com>
2020-06-19 15:53:13 +0300

    Add Gulp basic automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,41 @@
 const gulp = require('gulp');
+const server = require('browser-sync').create();
+const sass = require('gulp-sass');
+const del = require('del');
 
-gulp.task('copy', function () {
-  return gulp.src('./src/**')
+function clean (done) {
+  return del('dist', done);
+}
+
+function sassCompile () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(server.stream());
+}
+
+function copy () {
+  return gulp.src(['./src/js/**/*.js',
+                   './src/*.html',
+                   './src/assets/**',
+                   './src/fonts/**'],
+                   {base: './src/'})
     .pipe(gulp.dest('./dist'));
-});
+}
+
+function serve () {
+  server.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+
+  gulp.watch('./src/scss/**/*.scss', sassCompile);
+  gulp.watch('./src/*.html').on('change', server.reload);
+  gulp.watch('./src/js/**/*.js').on('change', server.reload);
+}
+
+exports.serve = serve;
+exports.sassCompile = sassCompile;
+exports.clean = clean;
+exports.copy = copy;
