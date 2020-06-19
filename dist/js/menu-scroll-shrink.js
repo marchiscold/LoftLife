eae60c1a99587057373aed53584ab50ae6a8a085
Pavel Martynov <martynovdev@gmail.com>
2020-06-19 15:56:56 +0300

    Remove dist from repository

--- dist/js/menu-scroll-shrink.js
+++
@@ -1,9 +0,0 @@
-$(window).on('scroll', function () {
-  let $headerBody = $('.header__body');
-
-  if (document.documentElement.scrollTop > 0) {
-    $headerBody.addClass('header__body_scrolled');
-  } else {
-    $headerBody.removeClass('header__body_scrolled');
-  }
-});
--- dist/js/ware-tabs.js
+++
@@ -1,8 +0,0 @@
-$('.ware-nav__item').click(function () {
-  $('.ware-nav__item').not($(this)).removeClass('active');
-  $(this).addClass('active');
-
-  let tabid = $(this).data('tabid');
-  $('.product__list').not($(tabid)).removeClass('active');
-  $(tabid).addClass('active');
-});
